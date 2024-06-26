f60d243af03729a5c16597638a289b5b218c9ce6
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-26 18:15:27 +0900

    Test 파일 생성

---
+++ src/test/java/com/yechan/usersever/member/controller/MemberControllerTest.java
@@ -0,0 +1,17 @@
+package com.yechan.usersever.member.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class MemberControllerTest {
+
+//    @Autowired
+//    private MockMvc mockMvc;
+
+}
