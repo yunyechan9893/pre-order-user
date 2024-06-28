fb4b08ca7fad9dea74064edb9bfc0b6fd5b6abcb
윤예찬 <125535111+yunyechan9893@users.noreply.github.com>
2024-06-28 16:28:48 +0900

    Test: 각 메서드에 맞는 테스트 코드 작성 완료

--- src/test/java/com/yechan/usersever/member/controller/MemberControllerTest.java
+++ src/test/java/com/yechan/usersever/member/controller/MemberControllerTest.java
@@ -1,17 +1,243 @@
 package com.yechan.usersever.member.controller;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.yechan.usersever.common.service.ResponseService;
+import com.yechan.usersever.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest({MemberController.class, ResponseService.class})
 public class MemberControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
 
+    @MockBean
+    MemberService memberService;
+
+    @Nested
+    @DisplayName("signup 메서드는")
+    class Signup {
+
+        @Test
+        @DisplayName("유효한 정보로 회원가입을 할 수 있다")
+        void success() throws Exception {
+
+            String requestJson = "{"
+                + "\"userId\":\"testUserId\","
+                + "\"password\":\"testPassword!1\","
+                + "\"email\":\"testEmail@xxxxx.com\","
+                + "\"phone\":\"01000000000\","
+                + "\"address\":\"TestAddress\","
+                + "\"authNumber\":\"000000\""
+                + "}";
+
+            mockMvc.perform(post("/member/signup")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 정보로 회원가입을 할 수 없다")
+        void fail() throws Exception {
+
+            String requestJson = "{"
+                + "\"userId\":\"test\","
+                + "\"password\":\"tes\","
+                + "\"email\":\"test\","
+                + "\"phone\":\"010\","
+                + "\"address\":\"Test\","
+                + "\"authNumber\":\"00\""
+                + "}";
+
+            mockMvc.perform(post("/member/signup")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("login 메서드는")
+    class Login {
+
+        @Test
+        @DisplayName("유효한 정보로 로그인 할 수 있다")
+        void success() throws Exception {
+
+            String requestJson = "{"
+                + "\"userId\":\"testUserId\","
+                + "\"password\":\"testPassword!1\""
+                + "}";
+
+            mockMvc.perform(post("/member/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 정보로 로그인 할 수 없다")
+        void fail() throws Exception {
+
+            String requestJson = "{"
+                + "\"userId\":\"test\","
+                + "\"password\":\"tes\""
+                + "}";
+
+            mockMvc.perform(post("/member/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("updateUserInfo 메서드는")
+    class UpdateUserInfo {
+        @Test
+        @DisplayName("유효한 정보로 비밀번호를 변경할 수 있다")
+        void success() throws Exception {
+
+            String requestJson = "{"
+                + "\"id\":1,"
+                + "\"address\":\"TestAddress\","
+                + "\"phone\":\"01000000000\""
+                + "}";
+
+            mockMvc.perform(patch("/member/info")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 정보로 회원 정보를 변경할 수 없다")
+        void fail() throws Exception {
+
+            String requestJson = "{"
+                + "\"id\":1,"
+                + "\"phone\":\"010\","
+                + "\"address\":\"Test\""
+                + "}";
+
+            mockMvc.perform(patch("/member/info")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("duplicatedMemberId 메서드는")
+    class DuplicatedMemberId {
+
+        @Test
+        @DisplayName("유효한 아이디로 중복 확인이 가능하다")
+        void success() throws Exception {
+
+            mockMvc.perform(get("/member/duplication?userId=testUserId"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 아이디로 중복 확인할 수 없다")
+        void fail() throws Exception {
+
+            mockMvc.perform(get("/member/duplication?userId=test"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("sendMail 메서드는")
+    class SendMail {
+
+        @Test
+        @DisplayName("유효한 이메일로 메세지를 전송할 수 있다")
+        void success() throws Exception {
+
+            String requestJson = "{"
+                + "\"email\":\"test@xxxxx.com\""
+                + "}";
+
+            mockMvc.perform(post("/member/mail")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 이메일로 메세지를 전송할 수 없다")
+        void fail() throws Exception {
+
+            String requestJson = "{"
+                + "\"email\":\"testxxxxx.com\""
+                + "}";
+
+            mockMvc.perform(post("/member/mail")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("verifyEmail 메서드는")
+    class VerifyEmail {
+
+        @Test
+        @DisplayName("유효한 데이터로 이메일 인증을 할 수 있다")
+        void success() throws Exception {
+
+            String requestJson = "{"
+                + "\"email\":\"test@xxxxx.com\","
+                + "\"certificationNumber\":\"000000\""
+                + "}";
+
+            mockMvc.perform(post("/member/mail/certification")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("유효하지 데이터로 이메일 인증을 할 수 없다")
+        void fail() throws Exception {
+
+            String requestJson = "{"
+                + "\"email\":\"testxxxxx.com\","
+                + "\"certificationNumber\":\"0000\""
+                + "}";
+
+            mockMvc.perform(post("/member/mail")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+        }
+    }
 }
--- src/test/java/com/yechan/usersever/service/MemberServiceTest.java
+++ src/test/java/com/yechan/usersever/service/MemberServiceTest.java
@@ -1,21 +1,239 @@
 package com.yechan.usersever.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
-/*
-* 시나리오
-*
-* 사용자가 서비스를 이용하기 위해 회원가입을 시도했다
-* 하지만 올바르지 않은 정보들을 나열하여 첫 회원가입은 실패했다
-* 다시 올바른 정보를 입력 후 회원가입에 시도했지만 이메일 인증을 하지 않아 실패했다
-* 세번째 시도는 아이디가 중복되어 회원가입에 실패했다
-* 네번째 시도는 모든 것을 올바르게 작성하여 가입에 성공했다
-*
-* 이제 가입에 성공했으니 로그인을 하려고 한다
-* 하지만 이전 로그인 했던 비밀번호가 생각나지 않아 올바르지 않은 비밀번호를 입력하게 됐다 그래서 실패했다
-* 두번째는 모든 것을 올바르게 작성하여 로그인에 성공하게 됐다
-*
-* */
-@SpringBootTest
-public class MemberServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.yechan.usersever.common.exception.MemberException;
+import com.yechan.usersever.member.domain.Member;
+import com.yechan.usersever.member.dto.AddressAndPhoneRequest;
+import com.yechan.usersever.member.dto.EmailAuthenticationRequest;
+import com.yechan.usersever.member.dto.EmailRequest;
+import com.yechan.usersever.member.dto.LoginRequest;
+import com.yechan.usersever.member.dto.MemberRequest;
+import com.yechan.usersever.member.dto.PasswordRequest;
+import com.yechan.usersever.member.repository.MemberRepository;
+import com.yechan.usersever.member.service.MemberService;
+import com.yechan.usersever.member.service.RedisService;
+import com.yechan.usersever.member.utils.AES;
+import java.time.Duration;
+import java.util.Optional;
+import javax.mail.internet.MimeMessage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    private static final String CHECK_COMPLETED = "TRUE";
+    private static final Duration TEN_MINUTES = Duration.ofMillis(1000L * 60L * 10L);
+    private static final Duration THREE_MINUTES = Duration.ofMillis(1000L * 60L * 3L);
+    private static final String AUTH_NUMBER = "000000";
+    private static final String EMAIL = "TEST@XXXX.com";
+    private static final String MEMBER_ID = "testMemberId";
+    private static final String PASSWORD = "!aaaa12345";
+    private static final String ADDRESS = "xxxx";
+    private static final String PHONE = "00000000000";
+    @Autowired
+    RedisService redisService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    AES aes;
+    @MockBean
+    MimeMessage mimeMessage;
+
+    @AfterEach
+    void rollback() {
+        memberRepository.deleteAll();
+        redisService.deleteValues(EMAIL);
+        redisService.deleteValues(AUTH_NUMBER);
+    }
+
+    @Test
+    void singup() {
+        MemberRequest memberRequest = new MemberRequest(
+            MEMBER_ID,
+            PASSWORD,
+            EMAIL,
+            PHONE,
+            ADDRESS,
+            AUTH_NUMBER
+        );
+
+        redisService.setValues(AUTH_NUMBER, CHECK_COMPLETED, TEN_MINUTES);
+        memberService.signup(memberRequest);
+
+        assertThat(memberRepository.findOneByMemberId(
+            aes.encode(MEMBER_ID))).isNotNull();
+    }
+
+    @Nested
+    @DisplayName("checkDuplicationMemberId 메서드는")
+    class CheckDuplicationMemberId {
+
+        @Test
+        @DisplayName("유효한 아이디로 중복 확인을 통과할 수 있다")
+        void notDuplicateMemberId() {
+            assertThatCode(() -> memberService.checkDuplicationMemberId(MEMBER_ID))
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("중복된 아이디를 찾아낼 수 있다")
+        void duplicateMemberId() {
+            singup();
+
+            assertThatThrownBy(() -> memberService.checkDuplicationMemberId(MEMBER_ID))
+                .isInstanceOf(MemberException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("sendMail 메서드는")
+    class SendMail {
+
+        @Test
+        @DisplayName("유효한 아이디로 중복 확인을 통과할 수 있다")
+        void notDuplicateMemberId() {
+            EmailRequest emailRequest = new EmailRequest(EMAIL);
+            assertThatCode(() -> memberService.sendMail(emailRequest))
+                .doesNotThrowAnyException();
+
+            assertThat(redisService.getValues(EMAIL)).isNotEqualTo("null");
+        }
+    }
+
+    @Test
+    @DisplayName("verifyEmail 메서드는 ")
+    void verifyEmail(){
+        EmailAuthenticationRequest request = new EmailAuthenticationRequest(
+            EMAIL,
+            AUTH_NUMBER
+        );
+
+        redisService.setValues(EMAIL, AUTH_NUMBER, THREE_MINUTES);
+        String authNumber = memberService.verifyEmail(request);
+
+        assertThat(authNumber).isEqualTo(AUTH_NUMBER);
+    }
+
+    @Nested
+    @DisplayName("login 메서드는")
+    class Login {
+
+        @Test
+        @DisplayName("아이디가 일치하지 않는다면 에러를 시킨다")
+        void notEquleMemberId() {
+            LoginRequest loginRequest = new LoginRequest(
+                MEMBER_ID,
+                PASSWORD
+            );
+
+            assertThatThrownBy(() -> memberService.login(loginRequest))
+                .isInstanceOf(MemberException.class);
+        }
+
+        @Test
+        @DisplayName("비밀번호가 일치하지 않는다면 에러를 시킨다")
+        void notEqulePassword() {
+            singup();
+
+            String diffrentPassword = PASSWORD + "a";
+            LoginRequest loginRequest = new LoginRequest(
+                MEMBER_ID,
+                diffrentPassword
+            );
+
+            assertThatThrownBy(() -> memberService.login(loginRequest))
+                .isInstanceOf(MemberException.class);
+        }
+
+        @Test
+        @DisplayName("아이디 혹은 비밀번호가 일치하면 로그인을 할 수 있다")
+        void login() {
+            singup();
+
+            LoginRequest loginRequest = new LoginRequest(
+                MEMBER_ID,
+                PASSWORD
+            );
+
+            assertThatCode(() -> memberService.login(loginRequest))
+                .doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    @DisplayName("updatePassword 메서드는")
+    class UpdatePassword {
+
+        @Test
+        @DisplayName("비밀번호를 업데이트 할 수 있다")
+        void notEquleMemberId() {
+            singup();
+            Long id = memberRepository.findOneByMemberId(aes.encode(MEMBER_ID));
+
+            String newPassword = PASSWORD + "a";
+            PasswordRequest passwordRequest = new PasswordRequest(
+                id,
+                newPassword
+            );
+
+            assertThatCode(() -> memberService.updatePassword(passwordRequest))
+                .doesNotThrowAnyException();
+
+            Optional<Member> member = memberRepository.findById(id);
+            assertThat(member.get().getPassword()).isEqualTo(newPassword);
+        }
+
+        @Test
+        @DisplayName("이전 비밀번호와 일치한다면 에러를 시킨다")
+        void notEqulePassword() {
+            singup();
+            Long id = memberRepository.findOneByMemberId(aes.encode(MEMBER_ID));
+
+            PasswordRequest passwordRequest = new PasswordRequest(
+                id,
+                PASSWORD
+            );
+
+            assertThatThrownBy(() -> memberService.updatePassword(passwordRequest))
+                .isInstanceOf(MemberException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("updateAddressAndPhone 메서드는")
+    class UpdateAddressAndPhone {
+
+        @Test
+        @DisplayName("비밀번호를 업데이트 할 수 있다")
+        void notEquleMemberId() {
+            singup();
+            Long id = memberRepository.findOneByMemberId(aes.encode(MEMBER_ID));
+
+            String newAddress = "yyyy";
+            String newPhone = "11111111111";
+            AddressAndPhoneRequest request = new AddressAndPhoneRequest(
+                id,
+                newPhone,
+                newAddress
+            );
+
+            assertThatCode(() -> memberService.updateAddressAndPhone(request))
+                .doesNotThrowAnyException();
+
+            Optional<Member> member = memberRepository.findById(id);
+            assertThat(member.get().getAddress()).isEqualTo(newAddress);
+            assertThat(member.get().getPhone()).isEqualTo(newPhone);
+        }
+    }
 }
